b4ec20922b05fd1e9ca78075b37c3f65bbc6805e
Siphephelo Myeni <sipheanele@gmail.com>
2023-08-25 00:50:52 +0200

    Service Implementation

--- src/main/java/siphephelo/server/service/ServerService.java
+++ src/main/java/siphephelo/server/service/ServerService.java
@@ -2,11 +2,13 @@ package siphephelo.server.service;
 
 import siphephelo.server.model.Server;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.Collection;
 
 public interface ServerService {
     Server create(Server server);
-    Server ping(String ipAddress);
+    Server ping(String ipAddress) throws IOException;
     Collection<Server> list(int limit);
     Server get(Long id);
     Server update(Server server);
--- src/main/java/siphephelo/server/service/implementation/ServerServiceImpl.java
+++ src/main/java/siphephelo/server/service/implementation/ServerServiceImpl.java
@@ -1,19 +1,45 @@
 package siphephelo.server.service.implementation;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import siphephelo.server.enumeration.Status;
 import siphephelo.server.model.Server;
+import siphephelo.server.repo.ServerRepo;
 import siphephelo.server.service.ServerService;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Collection;
 
+import static siphephelo.server.enumeration.Status.SERVER_DOWN;
+import static siphephelo.server.enumeration.Status.SERVER_UP;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class ServerServiceImpl implements ServerService {
+    private final ServerRepo serverRepo;
+
     @Override
     public Server create(Server server) {
-        return null;
+        log.info("Saving new Server: {}", server.getName());
+        String setServerImageUrl;
+        server.setImageUrl(setServerImageUrl());
+        return serverRepo.save(server);
     }
 
     @Override
-    public Server ping(String ipAddress) {
-        return null;
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging new server IP: {}", ipAddress);
+        Server server = serverRepo.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
     }
 
     @Override
@@ -35,4 +61,8 @@ public class ServerServiceImpl implements ServerService {
     public Boolean delete(Long id) {
         return null;
     }
+    private String setServerImageUrl() {
+        return null;
+    }
+
 }
